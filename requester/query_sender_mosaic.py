570d97f48ea2d83b91ff8fc29130a629cc3b5cfe
taey16 <taey1600@gmail.com>
2015-12-07 21:19:29 +0900

    add if response <> None: in requester/query_sender_mosaic.py

--- requester/query_sender_mosaic.py
+++ requester/query_sender_mosaic.py
@@ -7,12 +7,13 @@ import time
 import sys
 
 # input url list file
-#input_url_list = 'demo_all.txt.url.txt'
-input_url_list = 'demo_example.txt'
+input_url_list = 'demo_all.txt.url.txt'
+#input_url_list = 'demo_example.txt'
 # get url list
 urls = [entry.strip() for entry in open(input_url_list, 'r')]
 # set API prefix
-url_prefix = 'http://175.126.56.112:15003/mosaic_request_handler?url=%s'
+url_prefix = 'http://175.126.56.112:8080/mosaic_request_handler?url=%s'
+#url_prefix = 'http://175.126.56.112:15003/mosaic_request_handler?url=%s'
 
 #import pdb; pdb.set_trace()
 start_total = time.time()
@@ -21,13 +22,16 @@ for n, url in enumerate(urls):
     start_request = time.time()
     # get json object
     response = urllib2.urlopen(url_prefix % url)
-    # read json object into managable json object in python
-    retrieved_items = json.loads(response.read())
-    elapsed_start = time.time() - start_request
-    print '__org_img_url__, {}'.format(retrieved_items['__org_img_url__'])
-    print 'signature', retrieved_items['signature'][0:20]
-    print 'feature', retrieved_items['feature'][0:10]
-    print '%06d th sample, elapsed: %.4f msec.' % (n, elapsed_start)
+    if response <> None:
+      # read json object into managable json object in python
+      retrieved_items = json.loads(response.read())
+      elapsed_start = time.time() - start_request
+      print '__org_img_url__, {}'.format(retrieved_items['__org_img_url__'])
+      print 'signature', retrieved_items['signature'][0:20]
+      print 'feature', retrieved_items['feature'][0:10]
+      print '%06d th sample, elapsed: %.4f msec.' % (n, elapsed_start)
+    else:
+      print 'No response to url:', url
     sys.stdout.flush()
   except urllib2.HTTPError, err:
     if err.code == 404:
