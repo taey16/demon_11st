4e1f92829199b0bb2b312d34c252503cab020e45
taey16 <taey1600@gmail.com>
2015-12-09 09:02:25 +0900

    modify application.lua

--- lua/demon/application.lua
+++ lua/demon/application.lua
@@ -9,6 +9,7 @@ app.get("/mosaic_request_handler", function(req, res)
   local query_url = req.url.args.query or nil
   local q_cate = req.url.args.cate or nil
   local filename = paths.concat('/tmp/' .. tostring(torch.uniform())..'.jpg')
+  local result = {}
   if query_url then
     local wget_cmd = 'wget '..query_url..' -O '..filename
     os.execute(wget_cmd)
@@ -29,29 +30,28 @@ app.get("/mosaic_request_handler", function(req, res)
     feature = model.extract_feature(input, true)
     local table_scores = scores[{{1,5}}]:totable()
     local table_feature= feature:totable()
-    local result = {
+    result = {
       url = query_url,
       category = q_cate,
       name = class_name,
       score = table_scores,
       feature = table_feature,
       result = true} 
-    res.json(result)
 
     for i=1,5 do
       print(class_name[i] .. ' ' .. table_scores[i])
     end
   else
     --app.abort(400, 'request error', req, res)
-    local result = {
+    result = {
       url = query_url,
       category = q_cate,
       name = nil,
       score = nil,
       feature = nil,
       result = false} 
-    res.json(result)
   end
+  res.json(result)
 end)
 
 
@@ -60,4 +60,5 @@ options.host = '10.202.35.0'
 options.port = '8080'
 --app.repl({host='10.202.35.0', port='8081'})
 app.listen(options)
+
 
